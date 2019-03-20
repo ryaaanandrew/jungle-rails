1b74fd73b2ed6c43be96b9873ad8bbd2cb7a05ac
Ryan Andrew Sibat <ryanandrew.sibat@gmail.com>
2019-03-20 22:50:47 +0000

    created new admin page for catagories and set up routes

---
+++ app/controllers/admin/catagories_controller.rb
@@ -0,0 +1,4 @@
+class Admin::CatagoriesController < ApplicationController
+    def show
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -14,8 +14,13 @@ Rails.application.routes.draw do
 
   namespace :admin do
     root to: 'dashboard#show'
-    resources :products, except: [:edit, :update, :show]
+    resources :products, :catagories, except: [:edit, :update, :show]
   end
+
+  # namespace :admin do
+  #   root to: 'catagories#_catagories'
+  #   resources :products, except: [:edit, :update, :show]
+  # end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
