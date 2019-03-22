d1bd999f8d5dffd564ffb0771e458f143e549a8d
Ryan Andrew Sibat <ryanandrew.sibat@gmail.com>
2019-03-22 15:51:33 +0000

    created log in and register page, created user authentication and set up according routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,27 @@
 class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+  end
+  
+  def create 
+    user = User.new(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      email: params[:email],
+      password: params[:password],
+      password_confirmation: params[:password_confirmation]
+    )
+    if user.save
+      session[:user_id] = user.id
+      flash[:success] = "Successfully Created User!"
+      redirect_to '/'
+    else
+      flash[:warning] = "invalid Email or Password"
+      redirect_to '/sessions/new'
+    end
+  end
+
   def new
+    @user = User.new
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,7 @@ class User < ActiveRecord::Base
     validates :last_name, presence: true, length: { maximum: 50 }
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email, presence: true, length: { maximum: 255 },
-                format: { with: VALID_EMAIL_REGEX },
-                uniqueness: { case_sensitive: false }
+                format: { with: VALID_EMAIL_REGEX }
     has_secure_password
-    validates :password, presence: true, length: { minimum: 6 }
+    validates :password, presence: true
 end
